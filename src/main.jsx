a97266ac5a0acc75e957631f1f7c773dcb364ee0
flyingavius <arvidxian1029@gmail.com>
2025-04-21 01:05:49 -0400

    update: tailwindcss setting

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import 'src/index.css'
+import App from 'src/App.jsx'
+import 'tailwindcss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
